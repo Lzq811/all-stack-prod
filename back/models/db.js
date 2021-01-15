73423d69ade3f0c7ba645340d0656e37cffaaf7f
刘中奇 <liu.zhongqi@aquilaflycloud.com>
2021-01-15 11:39:34 +0800

    部署到alibaba 服务器

--- back/models/db.js
+++ back/models/db.js
@@ -1,4 +1,4 @@
 module.exports = app => {
   const mongoose = require('mongoose')
-  mongoose.connect('mongodb://demo:Lzq31311@3.129.46.226:27017/demo?authSource=demo&readPreference=primary&appname=MongoDB%20Compass&ssl=false', { useNewUrlParser: true, useUnifiedTopology: true })
+  mongoose.connect('mongodb://afc:afcroot@47.112.1.127:27017/demo?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false', { useNewUrlParser: true, useUnifiedTopology: true })
 }
